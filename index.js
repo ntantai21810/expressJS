09d856f8ca018bef52e487ed5e815255a5cc3e97
ntantai21810 <nguyentantai9a3@gmail.com>
2020-08-05 10:09:35 +0700

    Add query to search

--- index.js
+++ index.js
@@ -3,24 +3,35 @@ var app = express();
 
 var port = 3000;
 
+var users = [
+    { id: 1, name: 'Tai'},
+    { id: 2, name: 'Tan'}
+]
+
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-    // res.send("<h1>Hello Ryan</h1><a href = '/users'>Users list</a>");
     res.render('index.pug', {
         name: "Nguyen Tan Tai"
     });
 })
 
 app.get('/users', function(req, res) {
-    // res.send("User list");
     res.render('./users/index', {
-        users: [
-            { id: 1, name: 'Tai'},
-            { id: 2, name: 'Tan'}
-        ]
+        users: users
     });
+})
+
+app.get('/users/search', function(req, res) {
+    var name = req.query.name;
+    var matchListUsers = users.filter(function(user) {
+        return user.name.toLowerCase().indexOf(name.toLowerCase()) !== -1;
+    })
+    res.render('./users/index', {
+        name: name,
+        users: matchListUsers
+    })
 })
 
 app.listen(port, function() {
