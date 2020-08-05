a317149a092acd5815305ffae51c46e925b90dc8
ntantai21810 <nguyentantai9a3@gmail.com>
2020-08-05 09:01:13 +0700

    Use template engine, use pug

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ var app = express();
 
 var port = 3000;
 
-app.get('/', function(request, response) {
-    response.send("<h1>Hello Ryan</h1>");
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+    // res.send("<h1>Hello Ryan</h1><a href = '/users'>Users list</a>");
+    res.render('index.pug', {
+        name: "Nguyen Tan Tai"
+    });
 })
 
-app.get('/users', function(request, response) {
-    response.send("User list");
+app.get('/users', function(req, res) {
+    // res.send("User list");
+    res.render('./users/index', {
+        users: [
+            { id: 1, name: 'Tai'},
+            { id: 2, name: 'Tan'}
+        ]
+    });
 })
 
 app.listen(port, function() {
