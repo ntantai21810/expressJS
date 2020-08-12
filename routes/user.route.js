a15da4c5cd994d6051108cd4d52c30c87d2698bc
ntantai21810 <nguyentantai9a3@gmail.com>
2020-08-12 10:49:38 +0700

    Upload file to server

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
 
 var controller = require('../controller/user.controller');
-var validate = require('../validate/user.validate');  
+var validate = require('../validate/user.validate'); 
+
+var upload = multer({ dest: './public/uploads/' });
 
 router.get('/', controller.index);
 
@@ -15,7 +18,7 @@ router.get('/search', controller.search);
 
 router.get('/create', controller.create);
 
-router.post('/create', validate.postCreate, controller.postCreate);
+router.post('/create', upload.single('avatar'), validate.postCreate, controller.postCreate);
 
 router.get('/:id', controller.get);
 
